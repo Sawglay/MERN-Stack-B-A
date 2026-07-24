f66543232cb4580934eeb7ccb72a36454ca252ec
Sawglay <sawglay2@gmail.com>
2026-07-25 03:53:02 +0700

    Added node module

---
+++ Node/module1.js
@@ -0,0 +1,7 @@
+const myname = 'hlaingminhan';
+const myage = 23;
+
+module.exports = {
+    myname,
+    myage
+};
---
+++ Node/module2.js
@@ -0,0 +1,6 @@
+const obj = require('./module1')
+
+console.log(obj);
+console.log(obj.myage, obj.myname);
+
+
