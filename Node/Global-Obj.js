4632dca71d0a652a8907367f31b67d8aa1789936
Sawglay <sawglay2@gmail.com>
2026-07-24 15:14:43 +0700

    Added Global Object with necessities

---
+++ Node/Global-Obj.js
@@ -0,0 +1,19 @@
+// global.setTimeout(function(){
+//     console.log('Hi');
+    
+// }, 3000);
+
+let interval = global.setInterval(function(){
+    console.log('Bye');
+    
+}, 1000);
+
+global.setTimeout(function(){
+    clearInterval(interval)
+    
+}, 5000);
+
+
+console.log(__dirname);
+
+console.log(__filename);
