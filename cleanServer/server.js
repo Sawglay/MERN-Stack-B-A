e39b290b39e543e7b3495f03d2aaed9a5e16fcd1
Sawglay <sawglay2@gmail.com>
2026-07-25 16:41:00 +0700

    Added status code and redirect code in server.js

--- cleanServer/server.js
+++ cleanServer/server.js
@@ -10,7 +10,13 @@ const server = http.createServer((req, res)=>{
 
         case '/contact':
             filename = 'contact.html';
+            res.statusCode = 200;
             break;
+        
+         case '/contact-us':
+            res.statusCode = 301;
+            res.setHeader('Location', '/contact')
+            break;       
 
         case '/about':
             filename = 'about.html';
@@ -18,7 +24,7 @@ const server = http.createServer((req, res)=>{
     
         default:
             filename = '404.html';
-
+            res.statusCode = 404;
             break;
     }
 
