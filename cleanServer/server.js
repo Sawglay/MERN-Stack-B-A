f64f895946899b7a553bd941cede2b00cf5f6bc1
Sawglay <sawglay2@gmail.com>
2026-07-28 23:00:21 +0700

    Added lodash and updated package.json

--- cleanServer/server.js
+++ cleanServer/server.js
@@ -1,5 +1,9 @@
 const http = require('http');
 const fs = require('fs')
+const _ = require('lodash')
+
+console.log(_.random(20));
+
 
 const server = http.createServer((req, res)=>{
     let filename;
