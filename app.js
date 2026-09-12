7b98ab849df9a9965f9ce4898d43536b053b59f3
Sawglay <sawglay2@gmail.com>
2026-09-12 19:41:51 +0700

    All-blogs-routes-pasted-in-BlogRoutes

--- app.js
+++ app.js
@@ -65,34 +65,6 @@ app.get("/", async (req, res) => {
   res.redirect("/blogs");
 });
 
-app.get("/blogs", async (req, res) => {
-  // let blogs = [
-  //   {title : 'Blog Title 1', intro : 'This is blog intro 1'},
-  //   {title : 'Blog Title 2', intro : 'This is blog intro 2'},
-  //   {title : 'Blog Title 3', intro : 'This is blog intro 3'}
-  // ];
-  let blogs = await Blog.find().sort({ createdAt: -1 });
-  console.log(blogs);
-
-  res.render("home", {
-    blogs: blogs,
-    title: "Home",
-  });
-});
-
-app.post("/blogs", async (req, res) => {
-  let { title, intro, body } = req.body;
-
-  let blog = new Blog({
-    title,
-    intro,
-    body,
-  });
-
-  await blog.save();
-
-  res.redirect("/");
-});
 
 app.get("/contact", (req, res) => {
   res.render("contact", {
